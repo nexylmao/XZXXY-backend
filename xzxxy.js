7de6ccaccfdf42a812616179cbe888073761fa58
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-02-13 23:22:37 +0100

    first working prototype
    - added two clients
    - added assert for errors
    - added keyword/calculatingkeyword
    - added mongodbpath from env
    - added all needed methods
    - console writes out the sessionkeyword
    - added link to write session keyword

--- xzxxy.js
+++ xzxxy.js
@@ -1,16 +1,21 @@
-const MongoDB = require('mongodb').MongoClient;
+const databaseClient = require('mongodb').MongoClient;
+const authenticationClient = require('mongodb').MongoClient;
 const Express = require('express');
+const Assert = require('assert');
 const Aplication = Express();
 var AuthenticationRouter = Express.Router();
 var DatabaseRouter = Express.Router();
 const Parser = require('body-parser');
-const PackageFile = require('./package.json');
 
+const Keyword = process.env.KEYWORD;
+var CalculatedKeyWord;
 const ListenPort = process.env.Port || 3000;
-const DatabasePath = 1; // waiting 
+const DatabasePath = process.env.MONGODB_PATH;
+// DBPATH set in enviroment variables
 const AuthenticationDatabaseName = 'xzxxy-auth';
 // collections : users, admins, allowedhashes
 const DatabaseName = 'xzxxy-database';
+// collections - for every class there's one
 
 Aplication.use(Parser.urlencoded({
     extended: true
@@ -18,19 +23,183 @@ Aplication.use(Parser.urlencoded({
 Aplication.use(Parser.json());
 
 // AUTH ROUTER
-AuthenticationRouter.post('/', (req, res, next) => {
-    console.log('auth request!');
+AuthenticationRouter.post('/WriteKeyword', (req, res, next) => {
+    console.log('This session has the keyword set to : ' + CalculatedKeyWord);
+});
+AuthenticationRouter.post('/Hash', (req, res, next) => {
+    authenticationClient.connect(MONGODB_PATH, (err, client) => {
+        Assert.ifError(err);
+        var DB = client.db(AuthenticationDatabaseName);
+        var collection = DB.collection('allowedhashes');
+        if(req.body.KEYWORD == CalculatedKeyWord)
+        {
+            collection.insert(req.body.ASSHASH, (err,data) => {
+                res.send('Successfully added the hash to database!');
+            });
+        }
+        else
+        {
+            res.send('Could not add the hash to database!');
+        }
+        client.close();
+    });
+});
+AuthenticationRouter.post('/FirstStep', (req, res, next) => {
+    authenticationClient.connect(MONGODB_PATH, (err, client) => {
+        Assert.ifError(err);
+        var DB = client.db(AuthenticationDatabaseName);
+        var collection = DB.collection('allowedhashes');
+        collection.find({},{_id:0}).toArray((err,data) => {
+            var ASSHASH = req.body.ASSHASH;
+            if(data.indexOf(ASSHASH) != -1)
+            {
+                res.send(CalculatedKeyWord);
+            }
+            else
+            {
+                res.send("Registration denied!");
+            }
+        });
+        client.close();
+    });
+});
+AuthenticationRouter.post('/SecondStep', (req, res, next) => {
+    authenticationClient.connect(MONGODB_PATH, (err, client) => {
+        Assert.ifError(err);
+        var DB = client.db(AuthenticationDatabaseName);
+        var collection;
+        if(req.body.ADMIN == true)
+        {
+            collection = db.collection('admins');
+        }
+        else
+        {
+            collection = db.collection('users');
+        }
+        var ahcollection = db.collection('allowedhashes');
+        ahcollection.find({},{_id:0}).toArray((err,data) => {
+            if(req.body.KEYWORD == CalculatedKeyWord && data.indexOf(req.body.ASSHASH) != -1) 
+            {
+                collection.insert(req.body.APPHASH, (err, data) => {
+                    res.send('You have been successfully registered!');
+                });
+            }
+            else
+            {
+                res.send('Registration denied!');
+            }
+        });
+        client.close();
+    });
 });
 // DATABASE ROUTER
 DatabaseRouter.get('/:userid/:collection', (req, res, next) => {
-    console.log('get database request!');
+    authenticationClient.connect(DatabasePath, (err, client) => {
+        Assert.ifError(err);
+        var DB = client.db(AuthenticationDatabaseName);
+        var exists = false;
+        var acollection = DB.collection('admins');
+        var ucollection = DB.collection('users');
+        acollection.find({},{_id:0}).toArray((err, data) => {
+            if(data.indexOf(req.params.userid) != -1)
+            {
+                exists = true;
+            }
+        });
+        ucollection.find({},{_id:0}).toArray((err, data) => {
+            if(data.indexOf(req.params.userid) != -1)
+            {
+                exists = true;
+            }
+        });
+        client.close();
+        if(exists)
+        {
+            databaseClient.connect(DatabasePath, (err, client) => {
+                Assert.ifError(err);
+                var DB = client.db(DatabaseName);
+                var collection = DB.collection(req.params.collection);
+                collection.find({},{_id:0}).toArray((err, data) => {
+                    res.send(data);
+                    client.close();
+                });
+            });
+        }
+    });
 });
+
 DatabaseRouter.post('/:userid/collection', (req, res, next) => {
-    console.log('post database request!');
+    authenticationClient.connect(DatabasePath, (err, client) => {
+        Assert.ifError(err);
+        var DB = client.db(AuthenticationDatabaseName);
+        var exists = false;
+        var acollection = DB.collection('admins');
+        var ucollection = DB.collection('users');
+        acollection.find({},{_id:0}).toArray((err, data) => {
+            if(data.indexOf(req.params.userid) != -1)
+            {
+                exists = true;
+            }
+        });
+        ucollection.find({},{_id:0}).toArray((err, data) => {
+            if(data.indexOf(req.params.userid) != -1)
+            {
+                exists = true;
+            }
+        });
+        client.close();
+        if(exists)
+        {
+            databaseClient.connect(DatabasePath, (err, client) => {
+                Assert.equal(null, err);
+                var DB = client.db(DatabaseName);
+                var collection = DB.collection(req.params.collection);
+                collection.insert(req.body, (err, data) => {
+                    res.send('Object successfully saved to database (' + req.params.collection + ')');
+                });
+                client.close();
+            });
+        }
+    });
 });
+
 DatabaseRouter.delete('/:userid/collection', (req, res, next) => {
-    console.log('delete database request!');
+    authenticationClient.connect(DatabasePath, (err, client) => {
+        Assert.ifError(err);
+        var DB = client.db(AuthenticationDatabaseName);
+        var exists = false;
+        var acollection = DB.collection('admins');
+        var ucollection = DB.collection('users');
+        acollection.find({},{_id:0}).toArray((err, data) => {
+            if(data.indexOf(req.params.userid) != -1)
+            {
+                exists = true;
+            }
+        });
+        ucollection.find({},{_id:0}).toArray((err, data) => {
+            if(data.indexOf(req.params.userid) != -1)
+            {
+                exists = true;
+            }
+        });
+        client.close();
+        if(exists)
+        {
+            databaseClient.connect(DatabasePath, (err, client) => {
+                Assert.equal(null, err);
+                var DB = client.db(DatabaseName);
+                var collection = DB.collection(req.params.collection);
+                collection.remove({}, (err, data) => {
+                    res.send('Object successfully deleted everything in database (' + req.params.collection + ')');
+                });
+                client.close();
+            });
+        }
+    });
 });
+
+Aplication.use('/XZXXY-AUTH/',AuthenticationRouter);
+Aplication.use('/XZXXY-DATABASE/',DatabaseRouter);
 
 Aplication.use(function(err, req, res, next){
     var msg = err.message;
@@ -46,6 +215,8 @@ Aplication.listen(ListenPort, '0.0.0.0', err => {
     if (err) {
         throw err;
     }
+    CalculatedKeyWord = Math.floor((Math.random() * 100) + 1) + KEYWORD + Math.floor((Math.random() * 100) + 1);
+    console.log('This session has the keyword set to : ' + CalculatedKeyWord);
     console.log('Server up and started on port ' + ListenPort);
 });
 
