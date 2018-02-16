4221f1d99804a70df988b6a44f89d8aaaee1f0e0
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-02-16 22:35:32 +0100

    some more logging, im being real cancer to this github

--- xzxxy.js
+++ xzxxy.js
@@ -131,6 +131,7 @@ AuthenticationRouter.post('/SecondStep', (req, res, next) => {
             if(req.body.KEYWORD == CalculatedKeyWord && y) 
             {
                 console.log({user:req.body.APPHASH});
+                console.log(collection);
                 collection.insert({user:req.body.APPHASH}, (err, data) => {
                     res.send('You have been successfully registered!');
                 });
