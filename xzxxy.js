3516333cda46a59f9eaa0767a6b3c7adc7aafbf6
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-02-19 10:06:17 +0100

    hm?

--- xzxxy.js
+++ xzxxy.js
@@ -531,8 +531,8 @@ Aplication.use((err, req, res) =>
 	const error = err.error || err;
 	const status = err.status || 500;
 	res.status(status).json({
-		message: msg,
-		error
+        message: msg,
+        error: error
 	});
 });
 
